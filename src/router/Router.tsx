79266e3ef1ebd12c8a8e20fe1b380fe618cc8fb9
Levi_Utima <146796839+leviutima@users.noreply.github.com>
2024-07-11 19:11:14 -0300

    implementando fonte global e estrutura de rotas

---
+++ src/router/Router.tsx
@@ -0,0 +1,18 @@
+
+import { BrowserRouter, Routes } from 'react-router-dom'
+import './Router.css'
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+        <Routes>
+          
+        </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
